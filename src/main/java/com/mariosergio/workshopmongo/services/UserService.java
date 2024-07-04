c196a9797d96fda041c063982e4121eddee5782a
MarioSergio2 <ms940054@gmail.com>
2024-07-03 22:30:08 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/mariosergio/workshopmongo/services/UserService.java
+++ src/main/java/com/mariosergio/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.mariosergio.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,11 +28,27 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
+	
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = repo.findById(obj.getId()).orElse(null);
+		if(newObj != null) {
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		}
+		return null;
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
 	}
