32c5911dca31ff02abe46dd825c434b2c2303be5
MarioSergio2 <ms940054@gmail.com>
2024-07-03 21:41:23 -0300

    Insercao de usuario com POST

--- src/main/java/com/mariosergio/workshopmongo/services/UserService.java
+++ src/main/java/com/mariosergio/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mariosergio.workshopmongo.domain.User;
+import com.mariosergio.workshopmongo.dto.UserDTO;
 import com.mariosergio.workshopmongo.repository.UserRepository;
 import com.mariosergio.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -21,5 +22,13 @@ public class UserService {
 
 	public User findById(String id) {
 		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
 	}
 }
