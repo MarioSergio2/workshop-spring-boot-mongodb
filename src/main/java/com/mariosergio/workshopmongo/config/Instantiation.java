6c91b8258288cadfb5e0dec5c1c46990f1b946b9
MarioSergio2 <ms940054@gmail.com>
2024-07-09 22:39:09 -0300

    Referenciando os posts do usuario

--- src/main/java/com/mariosergio/workshopmongo/config/Instantiation.java
+++ src/main/java/com/mariosergio/workshopmongo/config/Instantiation.java
@@ -25,7 +25,6 @@ public class Instantiation implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// SimpleDateFormat configuration
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
@@ -38,14 +37,17 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+		// Save users
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		// Create posts
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem","Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-		// Save users and posts
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		// Save posts
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 }
