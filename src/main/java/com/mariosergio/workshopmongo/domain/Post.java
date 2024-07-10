a2f21fbe01037fce3c3b605ede6bf4d372c5ca18
MarioSergio2 <ms940054@gmail.com>
2024-07-10 15:28:41 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/mariosergio/workshopmongo/domain/Post.java
+++ src/main/java/com/mariosergio/workshopmongo/domain/Post.java
@@ -1,91 +1,104 @@
 package com.mariosergio.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.mariosergio.workshopmongo.dto.AuthorDTO;
+import com.mariosergio.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
-    private static final long serialVersionUID = 1L;
-    
-    @Id
-    private String id;
-    private Date date;
-    private String title;
-    private String body;
-    private AuthorDTO author;
-    
-    public Post() {
-    }
+	private static final long serialVersionUID = 1L;
 
-    public Post(String id, Date date, String title, String body, AuthorDTO author) {
-        super();
-        this.id = id;
-        this.date = date;
-        this.title = title;
-        this.body = body;
-        this.author = author;
-    }
+	@Id
+	private String id;
+	private Date date;
+	private String title;
+	private String body;
+	private AuthorDTO author;
 
-    public String getId() {
-        return id;
-    }
+	private List<CommentDTO> comments = new ArrayList<>();
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public Post() {
+	}
 
-    public Date getDate() {
-        return date;
-    }
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.title = title;
+		this.body = body;
+		this.author = author;
+	}
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public Date getDate() {
+		return date;
+	}
 
-    public String getBody() {
-        return body;
-    }
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
-    public void setBody(String body) {
-        this.body = body;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public AuthorDTO getAuthor() {
-        return author;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public void setAuthor(AuthorDTO author) {
-        this.author = author;
-    }
+	public String getBody() {
+		return body;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+	public void setBody(String body) {
+		this.body = body;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Post other = (Post) obj;
-        return Objects.equals(id, other.id);
-    }
+	public AuthorDTO getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(AuthorDTO author) {
+		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		return Objects.equals(id, other.id);
+	}
 }
