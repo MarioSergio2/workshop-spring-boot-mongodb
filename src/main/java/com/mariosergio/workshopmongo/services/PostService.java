08446dbc0da5362ed642cfe0ff9e1174c452a86a
MarioSergio2 <ms940054@gmail.com>
2024-07-10 15:50:16 -0300

    Consulta simples com query methods

--- src/main/java/com/mariosergio/workshopmongo/services/PostService.java
+++ src/main/java/com/mariosergio/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.mariosergio.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +17,8 @@ public class PostService {
 
 	public Post findById(String id) {
 		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
